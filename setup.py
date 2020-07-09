d862ff1c540b5171917d96173913549625d4cadb
microprediction <info@3za.org>
2020-07-09 09:27:12 -0400

    version bump ... first with CI

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="helicopter",
-    version="0.0.2",
+    version="0.0.3",
     description="Linking SciML Julia helicopter challenge to Microprediction.Org",
     long_description=README,
     long_description_content_type="text/markdown",
